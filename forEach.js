fe54d301b5a539f6d4dbef8e0dfe4bdcdd9a19c8
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-28 17:57:43 +0200

    update.forEach, console logs

--- forEach.js
+++ forEach.js
@@ -1,3 +1,8 @@
 const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
 
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
+
+names.forEach (name =>console.log(name));
+
+provinces.forEach (province =>console.log(province));
+
