82dd2e579003ed52e7d88016ccc5635065b850c7
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-28 17:31:27 +0200

    data sets being imported

---
+++ forEach.js
@@ -0,0 +1,3 @@
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
+
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
