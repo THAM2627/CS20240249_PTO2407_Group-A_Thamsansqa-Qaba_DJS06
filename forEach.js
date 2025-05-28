c97cbec2aa4fea5dc487e3201c3557dcbcc56607
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-28 17:58:00 +0200

    updating names database

--- forEach.js
+++ forEach.js
@@ -1,6 +1,6 @@
 const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
 
-const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
+const names = ['Ashwin', 'Sibongile', 'Jan-Hendrik', 'Sifso', 'Shailen', 'Frikkie'];
 
 names.forEach (name =>console.log(name));
 
