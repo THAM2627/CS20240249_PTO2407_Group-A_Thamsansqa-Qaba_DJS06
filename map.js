05052059b04cea34eb9ab7b19e952dc4345a8172
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-28 20:17:43 +0200

    updating map

---
+++ map.js
@@ -0,0 +1,5 @@
+const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
+
+const upperCaseProvinces = provinces.map(province => province.toUpperCase());
+
+console.log(upperCaseProvinces);
