8a8afc6f10472998a7ab38a92a76319433c5af01
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-29 18:03:13 +0200

    advanceExcercise logic

--- advanceExcercise.js
+++ advanceExcercise.js
@@ -6,3 +6,20 @@ const products = [
     { product: 'coffee', price: 10 },
     { product: 'tea', price: '' },
   ];
+
+  console.log(products.map(product => product.name));
+
+  console.log(products.filter(product => product.name.length <= 5));
+
+  console.log(
+    products
+    .filter(product => product.price)
+    .map(product => product.price)
+    .reduce((acc, price) => acc + price, 0)
+  );
+
+  console.log(
+      products.reduce((acc, product) => acc + product.name, '')
+  )
+
+  
