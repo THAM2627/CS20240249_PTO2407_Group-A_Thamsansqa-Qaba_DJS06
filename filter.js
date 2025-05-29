c9ca4d4e411d16ab6e7d2eb85402b8531302435b
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-29 12:56:54 +0200

    filtered provinces logic

--- filter.js
+++ filter.js
@@ -1,1 +1,5 @@
 const provinces = ['Western Cape', 'Gauteng', 'Northern Cape', 'Eastern Cape', 'KwaZulu-Natal', 'Free State'];
+
+const filteredProvinces = provinces.filter(province => province.includes('Cape'));
+
+console.log(filteredProvinces.length);
