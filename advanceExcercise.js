963b41f2816853b23823585d96a38c1c8ef2515a
Thamsanqa Qaba <thamie.qaba@gmail.com>
2025-05-31 20:59:12 +0200

    creating new lines

--- advanceExcercise.js
+++ advanceExcercise.js
@@ -8,19 +8,19 @@ const products = [
   ];
 
   console.log (
-    `${products.map(product => product.product)}` + 
+    `${products.map(product => product.product)}\n` + 
 
-    `${products.filter(product => product.product.length <= 5)}` +
+    `${products.filter(product => product.product.length <= 5)}\n` +
 
     `${products
     .filter(product => product.price)
     .map(product => product.price)
-    .reduce((acc, price) => acc + price, 0)}` +
+    .reduce((acc, price) => acc + price, 0)}\n` +
 
-    `${products.reduce((acc, product) => acc + product.product, '')}` +
+    `${products.reduce((acc, product) => acc + product.product, '')}\n` +
 
     `Highest: ${Math.max(...products.map(product => product.price))}. 
-    Lowest: ${Math.min(...products.map(product => product.price))}`+
+    Lowest: ${Math.min(...products.map(product => product.price))}\n`+
 
     `${products.reduce((acc, product) => ({ ...acc, [product.product]: { name: product.product, cost: product.price } }), {})}`
   )
